dd5c711c7fa9d1e753310c508afe709db900dff6
ivo.junior <ivo.junior@ambevtech.com.br>
2022-05-13 13:58:08 -0300

    alter para testar ponteiros e var explicita

--- Hello.go
+++ Hello.go
@@ -3,18 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	nome := "Douglas"
-	idade := 24
-	versao := 1.1
+	nome := "Ivo"
+	idade := 19
+	versao := 1.2
+
 	fmt.Println("Olá sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
 
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
-
+	fmt.Println("1- digite um numero:")
 	var comando int
 	fmt.Scanf("%d", &comando)
 
-	fmt.Println("O comando escolhido foi", comando)
+	fmt.Println("2- digite outro numero:")
+	var comando2 int
+	fmt.Scan(&comando2)
+
+	fmt.Println("O primeiro numero escolhido foi", comando)
+	fmt.Println("O segundo numero escolhido foi", comando)
 }
